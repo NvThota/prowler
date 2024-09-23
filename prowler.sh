ddf73cbd83bea23444894ec75541e737dc7d35d3
NvThota <88842720+NvThota@users.noreply.github.com>
2024-09-23 11:53:57 +0530

    Create prowler.sh

---
+++ prowler.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update the system
+yum update -y
+
+# Configure AWS CLI
+aws configure
+
+# Install Python3 and pip
+yum install -y python3-pip
+
+# Install setuptools using pip
+pip3 install setuptools
+
+# Install Prowler using pip
+pip3.9 install prowler
+
+# Add local bin to PATH
+export PATH=$PATH:/home/$HOME/.local/bin/
+
+# Run Prowler to check the version
+prowler -v
+
+# Run Prowler on AWS S3 service
+prowler aws --services s3
+
+# Install git
+yum install -y git
+
+# Clone the Prowler repository
+git clone https://github.com/prowler-cloud/prowler.git
+
+echo "Prowler setup completed!"
